623b583a40c1f7f1997c0287ecccfbfa7909f89a
risrobs <robertivanov22@abv.bg>
2021-12-24 12:48:32 +0200

    sss

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,10 @@ window.addEventListener("DOMContentLoaded", () => {
     alert("💣");
   });
 });
+document.getElementById('dark-theme-button').addEventListener('click', () => {
+  document.documentElement.style.setProperty('--background-color', '#333')
+})
+
+document.getElementById('light-theme-button').addEventListener('click', () => {
+  document.documentElement.style.setProperty('--background-color', 'white')
+})
